4f59326d9074205abd54905dfb9f6a001ab14552
Bhumitkhatri <87858149+Bhumitkhatri@users.noreply.github.com>
2021-07-26 16:56:53 +0530

    Update arrayMethod.js

--- arrayMethod.js
+++ arrayMethod.js
@@ -48,4 +48,32 @@ console.log(greaterNum);
 let arr = [1,2,3,4,5,6,7];
 let found = arr.find(element => element > 5);
 console.log(found);
+
+//reverse
+
+let arr = [1,2,3,4,5];
+arr.reverse();
+console.log(arr);
+
+//sort()
+
+let arr = [1,2,3,4,5,6];
+let alpha = ['a','e','q','r'];
+//sort in descending order
+descOrder = arr.sort((a, b) => a > b ? -1 : 1);
+console.log(descOrder);
+//sort in accending order
+ascOrder = alpha.sort((a, b) => a > b ? 1 : -1);
+console.log(ascOrder);
+
+
+//array.from
+let Cname = 'DsCode';
+let CnameArray = Array.from(Cname);
+console.log(CnameArray);
+
+
+//array.of
+let num = Array.of(1,2,3,4,5,6);
+console.log(num);
 */
