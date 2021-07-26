1813ed3877156226b83e21a12a7c4cf2dd85c458
Bhumitkhatri <87858149+Bhumitkhatri@users.noreply.github.com>
2021-07-26 12:11:15 +0530

    Add files via upload

---
+++ arrayMethod.js
@@ -0,0 +1,45 @@
+
+//forEach()
+
+/*let arr=[1,2,3,4,5];
+arr.forEach(item => {
+    console.log(item);
+});
+
+//includes()
+
+let arr=[1,2,3,4,5];
+console.log(arr.includes(3));
+console.log(arr.includes(7));
+
+//filter()
+
+let arr=[1,2,3,4,5];
+let filtered = arr.filter(num => num>3);
+console.log(filtered);
+
+//map()
+
+let arr=[1,2,3,4,5];
+let oneAdded = arr.map(num => num + 10);
+console.log(oneAdded);
+
+
+//Reduce()
+let arr = [1,2,3,4,5,6];
+let reduced = arr.reduce((total,current) => total + current);
+console.log(reduced);
+
+
+//some()
+
+let arr = [1,2,3,4,5,6,7];
+let greaterNum = arr.some(num => num>5);
+console.log(greaterNum);
+
+
+//every()
+let arr = [1,2,3,4,5,6,7];
+let greaterNum = arr.every(num => num>5);
+console.log(greaterNum);
+*/
