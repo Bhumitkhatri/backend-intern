d2ef0b1c9a1e5593c9006f12a149e3bf82025171
Bhumitkhatri <87858149+Bhumitkhatri@users.noreply.github.com>
2021-07-26 11:50:50 +0530

    Add files via upload

---
+++ askquestion.js
@@ -0,0 +1,15 @@
+function ask(question, yes, no) {
+    if (confirm(question)) yes()
+    else no();
+  }
+  
+  function showOk() {
+    console.log( "You agreed." );
+  }
+  
+  function showCancel() {
+    console.log( "You canceled the execution." );
+  }  
+  
+  // usage: functions showOk, showCancel are passed as arguments to ask
+  ask("Do you agree?", showOk, showCancel);
---
+++ assignment.js
@@ -0,0 +1,7 @@
+let a = 1;
+let b = 2;
+
+let c = 3 - (a = b + 1);
+
+console.log( a ); // 3
+console.log( c ); // 0
