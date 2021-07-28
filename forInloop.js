db0e27cf0ae422301df191647f1614cb4fceda9d
Bhumitkhatri <87858149+Bhumitkhatri@users.noreply.github.com>
2021-07-28 15:01:09 +0530

    Add files via upload

---
+++ forInloop.js
@@ -0,0 +1,9 @@
+let obj = {
+    firstName : "ysh",
+    lastName : "kri",
+    Age : 23,
+    email : "hello@yashkhatri.com"
+};
+for (let key in obj) {
+    console.log(key + " : " + obj [key] );
+}
---
+++ forOfLoop.js
@@ -0,0 +1,5 @@
+let array1 = ['a', 'b', 'c'];
+
+for (let name of array1) {
+  console.log(name);
+}
