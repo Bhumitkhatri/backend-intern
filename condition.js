32b7c4185595c79443e5ace61d9e0e84265e00e1
Bhumitkhatri <87858149+Bhumitkhatri@users.noreply.github.com>
2021-08-03 10:01:00 +0530

    Update condition.js

--- condition.js
+++ condition.js
@@ -1,11 +1,17 @@
 var marksObtained = window.prompt('please enter the marks obtained');
 if (marksObtained!="number") {
-    console.log('invalid input');
+    alert('invalid input');
 } else if (marksObtained < 0 || marksObtained > 100) {
-    console.log('Input a number between 0 to 100');
-} else {    var totalMarks = 100;
+    alert('Input a number between 0 to 100');
+} else {
+    marksObtained = Number(marksObtained);
+    var totalMarks = 100;
     var percentageOfMarksObtained = (marksObtained / totalMarks)*100;
 
-    isTopper = (percentageOfMarksObtained > 90)?true:false
-    console.log (isTopper)
+    if (percentageOfMarksObtained > 90) {
+        isTopper = true;
+    } else {
+        isTopper = false;
+    }
+    alert (isTopper)
 }
